08d1418b6a484815a8425db93993140d85fd9c1c
NoSkilz <radek.simicek456@gmail.com>
2017-10-15 23:22:28 +0200

    some message

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -11,8 +11,8 @@ class HomepagePresenter extends Nette\Application\UI\Presenter
     }
     public function renderDefault()
     {
-        $this->template->bests = $this->database->table('product')->select('product_id,product_name,platform_name,price')->order('sold')->limit(20);
-        $this->template->news = $this->database->table('product')->select('product_id,product_name,platform_name,price,picture,description')->order('uploaded')->limit(8);
+        $this->template->bests = $this->database->table('product')->select('product_id,product_name,platform_name,price')->order('sold DESC')->limit(20);
+        $this->template->news = $this->database->table('product')->select('product_id,product_name,platform_name,price,picture,description')->order('uploaded DESC')->limit(8);
         $this->template->platforms = $this->database->table('platform')->select('platform_name');
         $this->template->genres = $this->database->table('genre')->select('genre_name');
     }
